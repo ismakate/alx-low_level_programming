25a32a70f93c01a8de3e0c1f096712b97bbc5149
ismakate <ismakate@gmail.com>
2022-12-09 01:28:06 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/** Write a program that prints all possible combinations of two two-digit numbers.
+ * numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * The combination of numbers must be separated by comma, followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * 00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+													}
+	}
+	putchar('\n');
+	return (0);
+}

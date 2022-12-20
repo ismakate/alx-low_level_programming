28fbc138268faf23d97a891c7928fc0ab8cd3454
ismakate <ismakate@gmail.com>
2022-12-20 04:51:33 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - function resets int variable value to 98.
+ *
+ *@n: represent the int pointer
+ *
+ * *Return: Always 0 (Success)
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

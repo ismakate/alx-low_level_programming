c76580eb9e72555fae3a32cecd73040a27c19bf8
ismakate <ismakate@gmail.com>
2022-12-21 22:59:08 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strncat - function concatenates two strings
+ *
+ *@src: char pointer
+ *@dest: char pointer
+ *@n: int for bytes
+ *
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 *
+	 * src does not need to be null terminated
+	 *
+	 * if it contains n or more bytes
+	 */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
+	return (dest);
+}

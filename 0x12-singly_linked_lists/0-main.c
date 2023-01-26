11e63605ecb32fef04779211ccf4d83843ad0610
ismakate <ismakate@gmail.com>
2023-01-26 03:56:14 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -1,39 +1,35 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
- * Return: Always 0.
+ * print_list - print elements in a linked list
+ * @h: pointer to head first node
+ * Return: Number of nodes
  */
-int main(void)
+
+size_t print_list(const list_t *h)
 {
-list_t *head;
-list_t *new;
-list_t hello = {"World", 5, NULL};
-size_t n;
+	size_t node_count = 1;
 
-head = &hello;
-new = malloc(sizeof(list_t));
-if (new == NULL)
-{
-printf("Error\n");
-return (1);
-}
-new->str = strdup("Hello");
-new->len = 5;
-new->next = head;
-head = new;
-n = print_list(head);
-printf("-> %lu elements\n", n);
+	/*return 0 as number of nodes*/
+	if (h == NULL)
+		return (0);
 
-printf("\n");
-free(new->str);
-new->str = NULL;
-n = print_list(head);
-printf("-> %lu elements\n", n);
+	while (h->next != NULL)
+	{
+		/*if str is NULL return len as 0 and str as (nil)*/
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-free(new);
-return (0);
+		/*go to the next node*/
+		h = h->next;
+		/*count number of nodes*/
+		node_count += 1;
+	}
+
+	/*print for the last node*/
+	printf("[%d] %s\n", h->len, h->str);
+
+	return (node_count);
 }
